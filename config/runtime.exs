16049d0b3c6e58e617c292d288ad9d8379f2c1a0
Krzysztof Radecki <krzysztof.radecki@dac.digital>
2026-04-09 16:28:30 +0200

    Revert "fix: init Dotenvy with System.get_env() in prod — Dotenvy.env! needs it"
    This reverts commit b0f4d947ee0a2c50feb94236c2a521e235207264.

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,9 +2,6 @@ import Config
 
 if config_env() in [:dev, :test] do
   Dotenvy.source!([".env", System.get_env()])
-else
-  # In prod, Dotenvy still needs initialization for Dotenvy.env! calls below
-  Dotenvy.source!([System.get_env()])
 end
 
 # config/runtime.exs is executed for all environments, including
